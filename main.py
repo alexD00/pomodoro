9c2f91c62eea7fd95ba9bbf846ff4bc87443f003
Alex Rodriguez <alex0807miami@gmail.com>
2024-05-11 07:14:33 -0400

    set up basic ui

--- main.py
+++ main.py
@@ -19,13 +19,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=100)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.pack()
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
+
+canvas.pack()
 
 
 
